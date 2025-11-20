c05be9d301d3f762fdaf6cc9e6501128ea40725d
Eva Doumbe Ngangue <eva.doumbe-ngangue@epita.fr>
2025-11-20 14:43:17 +0100

    juste structure

---
+++ projet_eva_doumbe-ngangue/src/main/java/org/example/projet_eva_doumbengangue/ProjetEvaDoumbeNgangueApplication.java
@@ -0,0 +1,13 @@
+package org.example.projet_eva_doumbengangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEvaDoumbeNgangueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetEvaDoumbeNgangueApplication.class, args);
+    }
+
+}
---
+++ projet_eva_doumbe-ngangue/src/test/java/org/example/projet_eva_doumbengangue/ProjetEvaDoumbeNgangueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projet_eva_doumbengangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEvaDoumbeNgangueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
